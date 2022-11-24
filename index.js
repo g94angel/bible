bcb7bc2c44a9cb4cae6adc9a95c81a1aaa15aa9d
Angel Giron <angel.c.giron1@gmail.com>
2022-11-24 11:25:08 -0500

    added search feature

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-// const verseRefs = [
-//   'rom9:20',
-//   'rom10:9',
-//   'rom10:10',
-//   '2cor5:14',
-//   'rom 14:8',
-//   'rom 10:4',
-//   'matt 11:28',
-//   'john 6:37',
-//   'matt 6:33',
-//   'psalm 119:105',
-//   'col 3:2',
-//   'eph 4:32',
-//   'colossians 3:23',
-//   '2 tim 2:4',
-//   'john 1:4',
-//   'rev 22:12',
-//   'luke 6:31',
-//   'phil 4:4',
-//   'luke 17:32',
-// ];
-// const randomNum = Math.floor(Math.random() * verseRefs.length);
-
 function findVerse() {
-  console.log('finding verse');
-  // fetch(
-  //   `https://api.lsm.org/recver.php?String=${$(
-  //     '.verse-ref-input'
-  //   ).val()}&Out=json`
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //     // const ref = data.verses[0].ref;
-  //     // const text = data.verses[0].text.replace(/[\[\]/;]+/g, '');
-  //     // const subheading = document.querySelector('h3');
-  //     // subheading.innerText = `${text}`;
-  //   })
-  //   .catch((err) => console.log(err));
+  const reference = $('.verse-ref-input').val();
+  if (!reference) return;
+  $('.verse-ref-input').val('');
+  fetch(`https://api.lsm.org/recver.php?String=${reference}&Out=json`)
+    .then((res) => res.json())
+    .then((data) => {
+      $('.verses').empty();
+      if (data.verses.length > 1) {
+        console.log(data.verses);
+        $('.subheader').text('Only 30 verses can be displayed at a time.');
+        for (let i = 0; i < data.verses.length; i++) {
+          const ref = data.verses[i].ref;
+          const text = data.verses[i].text.replace(/[\[\]/;]+/g, '');
+          const message = `${ref} - ${text}`;
+
+          $('.verses').append(`<p>${message}</p>`);
+        }
+      } else {
+        const ref = data.verses[0].ref;
+        const text = data.verses[0].text.replace(/[\[\]/;]+/g, '');
+        const message = `${ref} - ${text}`;
+        $('.verses').append(`<p>${message}</p>`);
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 $('.search-btn').click(findVerse);
+$('.verse-ref-input').on('keypress', (e) => {
+  if (e.key === 'Enter') {
+    findVerse();
+  }
+});
