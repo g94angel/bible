8bac73de3b09c308562dd76f768d9aacacf010fe
Angel Giron <angel.c.giron1@gmail.com>
2023-01-18 20:18:48 -0800

    adding save search feature

--- index.js
+++ index.js
@@ -3,16 +3,67 @@ function reset() {
   $('.verse-ref-input').val('');
   $('.warning').text('').hide();
 }
-// const userSearches = [];
+
+let previousVisible = false;
+
 function logSearches(reference) {
-  $('.previous-searches-container')
-    .show()
-    .append(`<p class="reference">${reference}</p>`);
+  $('.previous').append(
+    `
+      <div class="ref-container">
+        <p class="reference">
+          ${reference}
+        </p>
+        <button class="btn save-btn" id="${reference}">
+          Save search
+        </button>
+      </div>
+    `
+  );
+  $('.save-btn').click(function () {
+    console.log('saving', reference);
+    // localStorage.setItem('test', this.id);
+    $('.saved').append(
+      `
+        <div class="ref-container">
+          <p class="reference">
+            ${reference}
+          </p>
+          <button class="btn search-verses-btn" id="${reference}">
+            Search verses
+          </button>
+        </div>
+      `
+    );
+    $('.search-verses-btn').click(function () {
+      // console.log(this.id);
+      console.log('searching for verses');
+      console.log(this.id);
+      findVerse(this.id);
+    });
+    if ($('.saved').is(':hidden')) {
+      $('.saved').show();
+    }
+  });
+  if ($('.previous').is(':hidden')) {
+    console.log('previous is hidden, will show now');
+    $('.previous').show();
+  }
 }
 
-function findVerse() {
-  const reference = $('.verse-ref-input').val();
-  if (!reference) return;
+function findVerse(input) {
+  // console.log('going to find a verse');
+  console.log('this is the input', input);
+  const ref = $('.verse-ref-input').val();
+  console.log('this is reference', ref);
+
+  // if (!ref || input) return;
+  const answer = input || ref;
+  console.log('answer', answer);
+  if (!answer) return;
+  fetchData(answer);
+}
+
+function fetchData(reference) {
   fetch(`https://api.lsm.org/recver.php?String=${reference}&Out=json`)
     .then((res) => res.json())
     .then((data) => {
@@ -57,4 +108,9 @@ $('.toggle-btn').click(() => {
   $('body').toggleClass('dark');
   $('input').toggleClass('dark');
   $('.copyright').toggleClass('dark');
+});
+
+$('.clear-btn').click(function () {
+  console.log('clearing storage');
+  // localStorage.clear();
 });
